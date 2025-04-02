441aada1aca5c8050a99ef972dda87593fed595e
brunotxrs <elielsonbrunno@gmail.com>
2025-04-02 13:03:01 -0400

    feat: import the component RendaScreen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
+import RendaScreen from './components/screens/RendaScreen';
 
 function App() {
   return (
     <div className="App">
-      
+
+      <RendaScreen />
+
     </div>
   );
 }
