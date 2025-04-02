849dc54caad635b0a311256df9b7b3ad27025ec4
brunotxrs <elielsonbrunno@gmail.com>
2025-04-02 16:38:03 -0400

    feat: import ContextProvider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ContextsProvider } from './components/contexts/Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ContextsProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ContextsProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
