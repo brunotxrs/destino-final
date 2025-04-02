ebf18784abb1983798e87eb349a1b0ce04a1ee99
brunotxrs <elielsonbrunno@gmail.com>
2025-04-02 16:39:51 -0400

    feat: import Contexts and useContext

--- src/components/ui/BotaoPrincipal.js
+++ src/components/ui/BotaoPrincipal.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 
 import '../css/BotaoPrincipal.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { Contexts } from "../contexts/Context"; 
 
 function BotaoPrincipal() {
+    const {objetivos, handleAddObjetivo, handleRemoveObjetivo, handleObjetivoChange } = useContext(Contexts);
 
-
+    console.log("Estado dos objetivos no BotaoPrincipal:", objetivos);
 
     return (
         <div className="container-botao">
@@ -15,15 +17,34 @@ function BotaoPrincipal() {
 
                 <fieldset className="box-fieldset">
                     
-                <legend>Objetivo</legend>
-                    <label for="">
-                        <input type="text"  placeholder="Reforma"/>
-                        <input type="number" placeholder="Valor R$"/>
+                <legend>Objetivos & Metas </legend>
+                    {objetivos.map((objetivo) => (
+                        <label for="" key={objetivo.id}>
+                        <input type="text"  placeholder="Objetivos"
+                        autoComplete="off"
+                        value={objetivo.texto}
+                        onChange={(e) => handleObjetivoChange(objetivo.id, 'texto', e.target.value)}
+                        />
+
+                        <input type="number" placeholder="Valor R$"
+                        autoComplete="off"
+                        value={objetivo.valor}
+                        onChange={(e) => handleObjetivoChange(objetivo.id, 'valor', e.target.value)}
+                        />
+
+                        <div className="box-icon">  
+                            <span className="icon-minus" onClick={() => handleRemoveObjetivo(objetivo.id)}>
+                                <FontAwesomeIcon icon={faMinus} />
+                            </span>
+                        </div>
+
+                        
                         
                     </label>
+                    ))}
 
                     <div className="add-more">
-                        <span className="icon-plus" >
+                        <span className="icon-plus" onClick={handleAddObjetivo}>
                             <FontAwesomeIcon icon={faPlus} />
                         </span>
                     </div>
