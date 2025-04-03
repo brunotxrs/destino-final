28c3de743c7bb40fd5b9b5366798cf15f9b5ca75
brunotxrs <elielsonbrunno@gmail.com>
2025-04-03 14:55:44 -0400

    feat: import next screen the DespesaScreen

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import ObjetivoScreen from './components/screens/ObjetivoScreen';
 import RendaScreen from './components/screens/RendaScreen';
 import Logo from './components/ui/Logo';
+import DespesaScreen from './components/screens/DespesaScreen';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path='/' element={<ObjetivoScreen />}/>
         <Route path='/renda-screen' element={<RendaScreen />} />
         <Route path='/objetivo-screen' element={<ObjetivoScreen />} />
+        <Route path='/despesa-screen' element={<DespesaScreen />} />
       </Routes>
       </BrowserRouter>
 
