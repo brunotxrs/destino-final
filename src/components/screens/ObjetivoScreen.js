3f661475baafad944126b627fcee3c44be5793b5
brunotxrs <elielsonbrunno@gmail.com>
2025-04-03 10:56:28 -0400

    feat: create new componente screen

---
+++ src/components/screens/ObjetivoScreen.js
@@ -0,0 +1,45 @@
+import React from "react";
+import ObjetivoInput from "../ui/ObjetivoInput"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+
+
+function ObjetivoScreen() {
+
+    const navigation = useNavigate()
+
+    const handleClick = () => {
+        
+        navigation('/renda-screen')
+
+    }
+
+    return (
+        <div className="container-screen-mobile">
+
+            <div className="info-mobile">
+                <p className="p-mobile">
+                    Descreva seus objetivos e as metas que deseja alcançar.
+                </p>
+                <p>
+                    <span className="span-mobile">
+                        (ex: "Comprar um carro")
+                    </span>
+                </p>
+            </div>
+
+            <ObjetivoInput />
+
+            <div className="box-icone-mobile">
+                <span className="ico-mobile" 
+                onClick={handleClick}><FontAwesomeIcon 
+                icon={faArrowRight} />
+                </span>
+            </div>
+
+        </div>
+    );
+};
+
+export default ObjetivoScreen;
