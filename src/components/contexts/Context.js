03add2ee7fd25722c9994cc4685986f182a0e14f
brunotxrs <elielsonbrunno@gmail.com>
2025-04-02 21:59:08 -0400

    feat: create new state

--- src/components/contexts/Context.js
+++ src/components/contexts/Context.js
@@ -9,6 +9,9 @@ export const ContextsProvider = (props) => {
     const [objetivos , setObjetivos] = useState([
         {id:0, texto:"", valor:""}
     ]);
+    const [rendas ,  setRendas] = useState([
+        {id:0, texto: "", valor:""}
+    ]);
     
     const [nextId , setNextId] = useState(1);
 
@@ -17,8 +20,17 @@ export const ContextsProvider = (props) => {
         setNextId(nextId + 1);
     };
 
+    const handleAddRenda = () => {
+        setRendas([...rendas, { id: nextId, texto:"", valor: "" }]);
+        setNextId(nextId + 1);
+    };
+
     const handleRemoveObjetivo = (idToRemove) => {
         setObjetivos(objetivos.filter(objetivo => objetivo.id !== idToRemove));
+    };
+
+    const handleRemoveRenda = (idToRemove) => {
+        setRendas(rendas.filter(renda => renda.id !== idToRemove))
     };
 
     const handleObjetivoChange = (id , field , value) => {
@@ -29,13 +41,25 @@ export const ContextsProvider = (props) => {
         setObjetivos(novosObjetivos);
     };
 
+    const handleRendaChange = (id, field, value) => {
+        const novasRendas = rendas.map((renda) => 
+        renda.id === id ? { ...renda, [field]: value} : renda
+    );
+
+    setRendas(novasRendas);
+    };
+
     return (
         <Contexts.Provider 
         value={{ 
             objetivos, 
             handleAddObjetivo, 
             handleRemoveObjetivo, 
-            handleObjetivoChange}}>
+            handleObjetivoChange, 
+            rendas, 
+            handleAddRenda, 
+            handleRemoveRenda, 
+            handleRendaChange}}>
                 {props.children}
         </Contexts.Provider>
     )
