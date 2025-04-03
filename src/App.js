6e11c6ef45ea80a74351c637296b2ed684a0cb7b
brunotxrs <elielsonbrunno@gmail.com>
2025-04-03 11:35:21 -0400

    feat: create new route for call screen ObjetivoScreen

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
      
         <Route path='/' element={<ObjetivoScreen />}/>
         <Route path='/renda-screen' element={<RendaScreen />} />
+        <Route path='/objetivo-screen' element={<ObjetivoScreen />} />
       </Routes>
       </BrowserRouter>
 
