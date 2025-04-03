7be930d7fc14b89905df98428b2aa7c5322953a4
brunotxrs <elielsonbrunno@gmail.com>
2025-04-03 14:50:45 -0400

    feat: create new component ui

---
+++ src/components/ui/DespesaInput.js
@@ -0,0 +1,71 @@
+import { useContext } from "react";
+import "../css/Input.css"
+import { Contexts } from "../contexts/Context"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+
+
+
+function DespesaInput() {
+    const {
+        despesas, 
+        handleAddDespesa, 
+        handleRemoveDespesa, 
+        handleDespesaChange
+    } = useContext(Contexts)
+
+
+    return(
+        <div className="container-input-mobile">
+
+            <form className="box-input-mobile">
+
+                <fieldset className="box-fieldset">
+                    <legend className="legend">Despesas</legend>
+
+                    {despesas.map((despesa)=> (
+                        <label className="label" key={despesa.id}>
+                            <input className="input" type="text" 
+                            placeholder="ex: Luz"
+                            autoComplete="off" 
+                            value={despesa.texto}
+                            onChange={(g) => 
+                                handleDespesaChange(despesa.id, "texto", g.target.value)
+                            }/>
+
+                            <input className="input" type="number" 
+                            placeholder="Valor R$"
+                            autoComplete="off" 
+                            value={despesa.valor}
+                            onChange={(g) => 
+                                handleDespesaChange(despesa.id, "valor", g.target.value)
+                            }/>
+
+                            <div className="box-icon-input-mobile">  
+                                <span className="icon-minus"onClick={() => handleRemoveDespesa(despesa.id)}><FontAwesomeIcon icon={faMinus} />
+                                </span>
+                            </div>
+
+                        </label>
+
+                    ))}
+
+
+                    <div className="add-more-mobile">
+                        <span className="icon-plus"
+                        onClick={handleAddDespesa}>
+                            <FontAwesomeIcon icon={faPlus} />
+                        </span>
+                    </div>
+
+                </fieldset>
+
+            </form>
+
+        </div>
+
+    );
+};
+
+
+export default DespesaInput;
