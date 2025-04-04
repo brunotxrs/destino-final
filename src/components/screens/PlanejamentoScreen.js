0becba424391ecf1d40d73b8c15eb0e42b6435fa
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-04-04 17:51:50 -0400

    feat: create new screen PlanejamentoScren

---
+++ src/components/screens/PlanejamentoScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ResultadoRenda from "../ui/ResultadoRenda";
+
+
+function PlanejamentoScreen() {
+
+
+    return (
+
+        <div className="container-planejamento">
+            <ResultadoRenda />
+
+        </div>
+    );
+};
+
+
+export default PlanejamentoScreen;
