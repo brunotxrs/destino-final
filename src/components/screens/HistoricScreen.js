6de0c13137ae8a55b352bd282a9de57a8645e5ea
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-04-04 11:38:27 -0400

    feat: create functions type show datas only if has, and conversion value for BRL

--- src/components/screens/HistoricScreen.js
+++ src/components/screens/HistoricScreen.js
@@ -13,17 +13,40 @@ function HistoricScreen() {
 
     const navigation = useNavigate()
 
-   const handleBackClick = () => {
+    const handleBackClick = () => {
         navigation('/despesa-screen')
     }
+
+    const temObjetivo = objetivos.some( objetivo => objetivo.valor !== "");
+
+    const resultadoRenda = rendas.reduce((abb, renda) => {
+        const valorNumerico = parseFloat(renda.valor) || 0;
+        return abb + valorNumerico;
+    }, 0);
+    
 
     const resultadoDespesas = despesas.reduce((acc, despesa) => {
         const valorNumerico = parseFloat(despesa.valor) || 0;
         return acc + valorNumerico
-    }, 0)
+    }, 0);
+    
 
+    const saldoLiquido = resultadoRenda - resultadoDespesas;
 
+    const temRenda = rendas.some(renda => renda.valor !== "");
+    const temDespesa = despesas.some(despesa => despesa.valor !== "");
+    const temSaldo = saldoLiquido !== 0;
 
+    const formatCurrency = (value) => {
+        return Number(value).toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+        });
+    };
+
+    
     return (
         <div className="container-historic">
             <h1 className="title">Suas Informações</h1>
@@ -31,36 +54,67 @@ function HistoricScreen() {
             <h2 className="sub-title">Seus Objetivos</h2>
             {objetivos.map((objetivo) => (
                 <div className="box-historic" key={objetivo.id}>
-                    <div className="box-objetivo">
-                        <span>{objetivo.texto}</span>
-                        <span>R$ {objetivo.valor}</span>
-                    </div>
+                    {temObjetivo && (
+                        <div className="box-objetivo">
+                            <span>{objetivo.texto}</span>
+                            <span>{formatCurrency(objetivo.valor)}</span>
+                        </div>
+                    )}
+                    
                 </div>
             ))}
             <h2 className="sub-title">Suas Rendas</h2>
             {rendas.map((renda) => (
                 <div className="box-historic" key={renda.id}>
-                    <div className="box-objetivo">
-                        <span>{renda.texto}</span>
-                        <span>R$ {renda.valor}</span>
-                    </div>
+                    {temRenda && (
+                        <div className="box-objetivo">
+                            <span>{renda.texto}</span>
+                            <span>{formatCurrency(renda.valor)}</span>
+                        </div>
+                    )}
+                    
                 </div>
             ))}
+            {temRenda && (
+                <div>
+                    <h3 className="sub-title-result">Resultado das Rendas:</h3>
+                    <div className="result">
+                        <span>{formatCurrency(resultadoRenda.toFixed(2))}</span>
+                    </div>
+                </div>
+            )}
+            
+            
             <h2 className="sub-title">Suas Despesas</h2>
             {despesas.map((despesa) => (
                 <div className="box-historic" key={despesa.id}>
-                    <div className="box-objetivo">
-                        <span>{despesa.texto}</span>
-                        <span>R$ {despesa.valor}</span>
-                    </div>
+                    {temDespesa && (
+                        <div className="box-objetivo">
+                            <span>{despesa.texto}</span>
+                            <span>{formatCurrency(despesa.valor)}</span>
+                        </div>
+                    )}
+                    
                 </div>
             ))}
-
-            <h3 className="sub-title-result">Resultado das Despesas:</h3>
-            <div className="test">
-                <span>R$ {resultadoDespesas.toFixed(2)}</span>
-            </div>
-
+            {temDespesa && (
+                <div>
+                    <h3 className="sub-title-result">Resultado das Despesas:</h3>
+                    <div className="result">
+                        <span>{formatCurrency(resultadoDespesas.toFixed(2))}</span>
+                    </div>
+                </div>
+            )}
+            
+            {temSaldo && (
+                <div>
+                    <h3 className="sub-title-result">Saldo Líquido</h3>
+                    <div className="result">
+                        <span>{formatCurrency(saldoLiquido.toFixed(2))}</span>
+                    </div>
+                </div>
+            )}
+            
 
             <div className="box-icone-mobile">
                 <span className="ico-mobile"
