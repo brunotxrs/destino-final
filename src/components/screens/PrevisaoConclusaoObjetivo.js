686235b59655212c264425fdc8ce32311027363f
Bruno Teixeira <183097017+brunotxrs@users.noreply.github.com>
2025-04-06 19:11:09 -0400

    feat: import ui Previsao and create function for back screen

--- src/components/screens/PrevisaoConclusaoObjetivo.js
+++ src/components/screens/PrevisaoConclusaoObjetivo.js
@@ -1,12 +1,30 @@
 import React from "react";
+import Previsao from "../ui/Previsao";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useNavigate } from "react-router-dom";
 
 
 function PrevisaoConclusaoObjetivo() {
 
+    const navigation = useNavigate();
+
+    const handleBackClick = () => {
+        navigation('/planejamento-screen')
+    };
 
     return (
         <div className="container-conclusao-objetivo">
+            <Previsao />
 
+            <div className="box-icone-mobile">
+                <span className="ico-mobile"
+                onClick={handleBackClick}
+                >
+                    <FontAwesomeIcon 
+                    icon={faArrowLeft} />
+                </span>
+            </div>
 
         </div>
     );
