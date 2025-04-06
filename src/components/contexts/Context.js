29136f81ff348ce86b1eae4eeaa1767c4eb74257
Bruno Teixeira <183097017+brunotxrs@users.noreply.github.com>
2025-04-06 12:23:46 -0400

    feat: create new states and context

--- src/components/contexts/Context.js
+++ src/components/contexts/Context.js
@@ -1,16 +1,14 @@
 import React, { createContext, useState } from "react";
 
-
 export const Contexts = createContext();
 
-
 export const ContextsProvider = (props) => {
-
-    const [objetivos , setObjetivos] = useState([]);
-    const [rendas ,  setRendas] = useState([]);
+    const [objetivos, setObjetivos] = useState([]);
+    const [rendas, setRendas] = useState([]);
     const [despesas, setDespesas] = useState([]);
-    
-    const [nextId , setNextId] = useState(0);
+    const [nextId, setNextId] = useState(0);
+    const [valorSeparado, setValorSeparado] = useState(0); 
+    const [porcentagemSelecionada, setPorcentagemSelecionada] = useState(null);
 
     const handleAddObjetivo = () => {
         setObjetivos([...objetivos, { id: nextId, texto: "", valor: "" }]);
@@ -18,12 +16,12 @@ export const ContextsProvider = (props) => {
     };
 
     const handleAddRenda = () => {
-        setRendas([...rendas, { id: nextId, texto:"", valor: "" }]);
+        setRendas([...rendas, { id: nextId, texto: "", valor: "" }]);
         setNextId(nextId + 1);
     };
 
     const handleAddDespesa = () => {
-        setDespesas([...despesas, { id: nextId, texto:"", valor: "" }]);
+        setDespesas([...despesas, { id: nextId, texto: "", valor: "" }]);
         setNextId(nextId + 1);
     };
 
@@ -32,56 +30,68 @@ export const ContextsProvider = (props) => {
     };
 
     const handleRemoveRenda = (idToRemove) => {
-        setRendas(rendas.filter(renda => renda.id !== idToRemove))
+        setRendas(rendas.filter(renda => renda.id !== idToRemove));
     };
 
     const handleRemoveDespesa = (idToRemove) => {
-        setDespesas(despesas.filter(despesa => despesa.id !== idToRemove))
+        setDespesas(despesas.filter(despesa => despesa.id !== idToRemove));
     };
 
-    const handleObjetivoChange = (id , field , value) => {
+    const handleObjetivoChange = (id, field, value) => {
+        const newValue = field === 'valor' ? Number(value) : value;
         const novosObjetivos = objetivos.map((objetivo) =>
-            objetivo.id === id ? { ...objetivo, [field]: value} : objetivo
+            objetivo.id === id ? { ...objetivo, [field]: newValue } : objetivo
         );
-
         setObjetivos(novosObjetivos);
     };
 
     const handleRendaChange = (id, field, value) => {
-        const novasRendas = rendas.map((renda) => 
-        renda.id === id ? { ...renda, [field]: value} : renda
-    );
-
-    setRendas(novasRendas);
+        const newValue = field === 'valor' ? Number(value) : value;
+        const novasRendas = rendas.map((renda) =>
+            renda.id === id ? { ...renda, [field]: newValue } : renda
+        );
+        setRendas(novasRendas);
     };
 
-
     const handleDespesaChange = (id, field, value) => {
-        const novasDespesas = despesas.map((despesa) => 
-        despesa.id === id ? { ...despesa, [field]: value} : despesa
-    );
+        const newValue = field === 'valor' ? Number(value) : value;
+        const novasDespesas = despesas.map((despesa) =>
+            despesa.id === id ? { ...despesa, [field]: newValue } : despesa
+        );
+        setDespesas(novasDespesas);
+    };
 
-    setDespesas(novasDespesas);
+    const setValorSeparadoContext = (valor) => {
+        setValorSeparado(valor);
+    };
 
+    const setPorcentagemSelecionadaContext = (porcentagem) => {
+        setPorcentagemSelecionada(porcentagem);
     };
 
     return (
-        <Contexts.Provider 
-        value={{ 
-            objetivos, 
-            handleAddObjetivo, 
-            handleRemoveObjetivo, 
-            handleObjetivoChange, 
-            rendas, 
-            handleAddRenda, 
-            handleRemoveRenda, 
-            handleRendaChange,
-            despesas, 
-            handleAddDespesa, 
-            handleRemoveDespesa, 
-            handleDespesaChange}}>
-                {props.children}
+        <Contexts.Provider
+            value={{
+                objetivos,
+                handleAddObjetivo,
+                handleRemoveObjetivo,
+                handleObjetivoChange,
+                rendas,
+                handleAddRenda,
+                handleRemoveRenda,
+                handleRendaChange,
+                despesas,
+                handleAddDespesa,
+                handleRemoveDespesa,
+                handleDespesaChange,
+                valorSeparado,
+                setValorSeparadoContext,
+                porcentagemSelecionada,
+                setPorcentagemSelecionadaContext, 
+                
+            }}
+        >
+            {props.children}
         </Contexts.Provider>
-    )
-
-}
+    );
+};
