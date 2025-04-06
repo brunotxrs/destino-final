afcc6660dae6b155d2404286c0461a70b0ca59cc
Bruno Teixeira <183097017+brunotxrs@users.noreply.github.com>
2025-04-05 21:04:51 -0400

    feat: create function for back screen

--- src/components/screens/PlanejamentoScreen.js
+++ src/components/screens/PlanejamentoScreen.js
@@ -1,14 +1,44 @@
 import React from "react";
+import '../css/PlanejamentoScreen.css'
 import ResultadoRenda from "../ui/ResultadoRenda";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 
 function PlanejamentoScreen() {
+    const navigation = useNavigate();
+
+    const handleBackClick = () => {
+        navigation('/historic-screen')
+    };
 
 
     return (
 
         <div className="container-planejamento">
-            <ResultadoRenda />
+            <div className="box-planejamento">
+                <h1>Planejamento</h1>
+                <ResultadoRenda />
+                <p className="oxi">Defina a base do seu planejamento automático: selecione uma porcentagem.</p>
+            </div>
+            
+
+
+
+            <div className="box-icone-mobile">
+                <span className="ico-mobile"
+                onClick={handleBackClick}
+                >
+                    <FontAwesomeIcon 
+                    icon={faArrowLeft} />
+                </span>
+                <span className="ico-mobile"
+                >
+                    <FontAwesomeIcon 
+                    icon={faArrowRight} />
+                </span>
+            </div>
 
         </div>
     );
