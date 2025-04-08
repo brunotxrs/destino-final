efce4f137df6e60c15ab32c2313640799036d901
Bruno Teixeira <183097017+brunotxrs@users.noreply.github.com>
2025-04-08 11:28:35 -0400

    feat: update componet Logo and update your styles

--- src/components/ui/Logo.js
+++ src/components/ui/Logo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../css/Logo.css"
-import logo from "../../assets/mobile/logo-destino-final-mobile.svg"
+import logo from "../../assets/mobile/logo.svg"
 
 function Logo() {
 
